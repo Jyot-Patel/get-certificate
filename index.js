79dc37df40d84ae47f16cb835fbd36a96bcbd806
Jyot-Patel <69694709+Jyot-Patel@users.noreply.github.com>
2021-01-18 16:57:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 
 
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
 $('#form').submit(function (e) {
     const namee = $('#name').val();
     if(!namee){
